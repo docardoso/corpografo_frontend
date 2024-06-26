919ef0884b8d3b727280c5a2138fa656417cb93c
Douglas Cardoso <douglascardoso@cos.ufrj.br>
2024-06-26 19:12:35 +0100

    Implementing create corpus

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from functools import partial
+
 import urllib.parse
 import os
 import requests
@@ -15,6 +17,11 @@ CORPOGRAFO_API_URL = os.getenv('CORPOGRAFO_API_URL', 'https://127.0.0.1:5000')
 VERIFY_SSL = os.getenv('VERIFY_SSL', False)
 
 unrestricted_page_routes = {'/login'}
+
+ui.menu_item.default_props('dense')
+ui.menu_item.default_classes('w-full')
+ui.input.default_classes('w-full')
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
@@ -33,23 +40,75 @@ def try_logout() -> None:
         headers={'Authorization': f'Bearer {app.storage.user.get("access_token")}'},
         verify=VERIFY_SSL,
     )
+    app.storage.user.clear()
     if r.status_code == 200:
-        app.storage.user.clear()
         ui.navigate.to('/login')
     else:
-        ui.notify(f'Operation failed: {r.json()["message"]}', color='negative')
+        try:
+            json = r.json()
+            ui.notify(f'Operation failed: {json.get("message", json)}', color='negative')
+        except requests.exceptions.JSONDecodeError:
+            ui.notify(f'Operation failed: unexpected behavior', color='negative')
 
 @ui.page('/')
 def main_page() -> None:
-    with ui.column().classes('absolute-center items-center'):
-        ui.label(f'Hello {app.storage.user["name"]}!').classes('text-2xl')
-        ui.button(on_click=try_logout, icon='logout') \
-            .props('outline round')
+    make_header_and_menu()
+
+def make_header_and_menu() -> None:
+    with ui.left_drawer(value=False).classes('bg-blue-100') as left_drawer:
+        ui.menu_item('Home', on_click=partial(ui.navigate.to, '/'))
+        ui.separator()
+
+        with ui.menu_item('Corpus'):
+            with ui.menu() as menu:
+                ui.menu_item('Create', on_click=partial(ui.navigate.to, '/create_corpus'))
+                ui.menu_item('Manage', on_click=partial(ui.navigate.to, '/manage_corpus'))
+        #for i in range(10):
+        #    #ui.label(f'Item {i}')
+        #    ui.menu_item(f'Item {i}', on_click=partial(ui.notify, f'Item {i}'))
+        ui.separator()
+        ui.menu_item('Logout', on_click=try_logout)
 
 
-@ui.page('/subpage')
-def test_page() -> None:
-    ui.label('This is a sub page.')
+    with ui.header().classes(replace='row items-center'):
+        ui.button(on_click=left_drawer.toggle, icon='menu').props('flat color=white')
+        ui.label(f'Hello, {app.storage.user["name"]}!') #.classes('text-2xl')
+
+
+@ui.page('/create_corpus')
+def create_corpus() -> None:
+    make_header_and_menu()
+    with ui.card().classes('w-96 absolute-center items-center'):
+        ui.label('New corpus: basic information')
+        corpus_name = ui.input('Name', validation={'Required': lambda value: len(value) > 0})
+        corpus_name.validate()
+        ui.button('Create', on_click=partial(try_create_corpus, corpus_name))
+
+def try_create_corpus(name):
+    r = requests.post(
+        urllib.parse.urljoin(CORPOGRAFO_API_URL, 'corpus'),
+        json={
+            'name': name.value,
+        },
+        headers={'Authorization': f'Bearer {app.storage.user.get("access_token")}'},
+        verify=VERIFY_SSL,
+    )
+
+    if r.status_code == 201:
+        ui.notify('Operation successful', color='positive')
+    else:
+        try:
+            json = r.json()
+            ui.notify(f'Operation failed: {json.get("message", json)}', color='negative')
+        except requests.exceptions.JSONDecodeError:
+            ui.notify(f'Operation failed: unexpected behavior', color='negative')
+
+
+
+@ui.page('/manage_corpus')
+def manage_corpus() -> None:
+    make_header_and_menu()
+    ui.label('MANAGE CORPUS.')
 
 async def try_login(email, password, client) -> None:
     #print('oi'*10)
@@ -136,14 +195,14 @@ def login(client: Client) -> Optional[RedirectResponse]:
         with ui.tab_panels(tabs, value='Login').classes('w-96'):
             with ui.tab_panel('Login'):
                 with ui.card().classes('w-full items-center'):
-                    email_login = ui.input('E-mail').classes('w-full')  #.on('keydown.enter', js_handler='alert("asaaoks");')
-                    password_login = ui.input('Password', password=True, password_toggle_button=True).classes('w-full')  #.on('keydown.enter', try_login)
+                    email_login = ui.input('E-mail')  #.on('keydown.enter', js_handler='alert("asaaoks");')
+                    password_login = ui.input('Password', password=True, password_toggle_button=True)  #.on('keydown.enter', try_login)
                     ui.button('Log in', on_click=lambda: try_login(email_login, password_login, client))
 
             with ui.tab_panel('Register'):
                 with ui.card().classes('w-full items-center'):
-                    register_name = ui.input('Name').classes('w-full')
-                    register_email = ui.input('E-mail').classes('w-full')
+                    register_name = ui.input('Name')
+                    register_email = ui.input('E-mail')
                     ui.button('Register', on_click=lambda: try_register(register_name, register_email, client))
 
             #with ui.tab_panel('Reset'):
